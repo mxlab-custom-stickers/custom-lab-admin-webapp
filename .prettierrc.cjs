396c549ba14ce025f5af936c63412103fa52519a
vlaude <vincent.laude2@gmail.com>
2025-03-27 11:38:39 +0100

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  plugins: [require.resolve('prettier-plugin-organize-imports')],
+};
